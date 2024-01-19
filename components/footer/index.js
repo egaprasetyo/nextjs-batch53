29a19676ae640a9d32cdca4c223a53dee484942c
Christianto Ega Prasetyo <dionisius.ega26@gmail.com>
2024-01-19 23:40:15 +0700

    feat: tugas hari-5

--- components/footer/index.js
+++ components/footer/index.js
@@ -2,8 +2,8 @@
 
 export default function Footer() {
   return (
-    <div className="h-12 text-lg font-bold bg-blue-600 text-center p-4">
-      Footer
+    <div className="h-12 text-lg font-bold bg-blue-600 text-center p-4 flex items-center justify-center">
+      <p>Footer</p>
     </div>
   );
 }
