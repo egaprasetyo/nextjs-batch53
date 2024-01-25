45124b301d0ecf85405036c16e93cef99e5a271b
egaprasetyo <ega@fleetify.id>
2024-01-25 09:56:09 +0700

    feat: tugas hari-9

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Home({ children }) {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <Layout
